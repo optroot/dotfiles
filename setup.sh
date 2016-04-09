122229ea333f1b083447161e5a17a997f8f1d0d1
optroot <optroot@gmail.com>
2016-04-09 02:18:12 -0400

    Added UltiSnips

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ mkdir -p ~/.vim
 mkdir -p ~/.vim/view
 mkdir -p ~/.vim/undo
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+rm -f ~/.vim/UltiSnips
+ln -s UltiSnips ~/.vim/UltiSnips
 
 link() {
   rm -f ~/.$1
