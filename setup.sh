c07db70104e01577a331d602fee90edd32050652
optroot <optroot@gmail.com>
2017-01-06 19:10:38 -0500

    Fri Jan  6 19:10:38 EST 2017

--- setup.sh
+++ setup.sh
@@ -11,17 +11,17 @@ mkdir -p ~/.vim/undo
 #curl -sfLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 rm -f ~/.vim/UltiSnips
-ln -s `readlink -e UltiSnips` ~/.vim/UltiSnips
+ln -s "$(readlink -e UltiSnips)" ~/.vim/UltiSnips
 
 link() {
-  echo $1
-  rm -fi ~/.$1
-  ln $1 ~/.$1
+  echo "$1"
+  rm -f "$HOME/.$1"
+  ln "$1" "$HOME/.$1"
 }
 
 cd files
 for f in *
 do
-    link $f
+    link "$f"
 done
 
