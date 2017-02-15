6546174e95bffacf06d003740ff566ce084958fd
optroot <optroot@gmail.com>
2017-02-15 18:25:43 -0500

    Update setup script

--- setup.py
+++ setup.py
@@ -7,34 +7,39 @@ import os
 import sys
 import difflib
 import filecmp
+import getopt
+
 try:
     from termcolor import colored
 except:
     colored = lambda x,y: x
 
+HOME = os.path.expanduser("~")
+settings = {
+    'dryrun': True,
+    'verbose': False,
+}
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-HOME = os.path.expanduser("~")
-DRY_RUN = False
+optlist, args = getopt.getopt(sys.argv[1:], 'dv', ['dryrun', 'verbose'])
+for opt,arg in optlist:
+    if opt in ['-d', '--dryrun']:
+        settings['dryrun'] = True
+        settings['verbose'] = True
+    if opt in ['-v', '--verbose']:
+        settings['verbose'] = True
 
-def mkdir(d):
-    if not os.path.exists(d):
-        if DRY_RUN: print 'mkdir -p', d
-        else: os.mkdirs(d)
 
 def home(p):
     return os.path.join(HOME, p)
 
-def link_files(src, dst, force=False, sym=False):
-    if force: 
-        if DRY_RUN: print 'rm', dst
-        else: os.remove(dst)
-    if sym:
-        if DRY_RUN: print 'ln -s', src, dst
-        else: os.symlink(src, dst)
-    else:
-        if DRY_RUN: print 'ln', src, dst
-        else: os.link(src, dst)
+def file_diff(src, dst):
+    pass
+
+def system(cmd):
+    if settings['verbose']: print cmd
+    if not settings['dryrun']: os.system(cmd)
 
 def get_user_choice(choices):
     keys = choices.keys()
@@ -49,50 +54,19 @@ def get_user_choice(choices):
         first = False
     return choice
 
-mkdir(home('.vim'))
-mkdir(home('.vim/view'))
-mkdir(home('.vim/undo'))
-
-# # TODO check if exists
-# # leave to vim
-# #curl -sfLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-# # TODO
-# # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-#
-# rm -f ~/.vim/UltiSnips
-# ln -s "$(readlink -e UltiSnips)" ~/.vim/UltiSnips
-
-if os.path.islink((home('.vim/UltiSnips'))):
-    link_files('UltiSnips', home('.vim/UltiSnips'), force=True, sym=True)
-    print colored('[Exists]'.ljust(12), 'yellow'), '%-24s'%'UltiSnips', '|', colored('Re-Linked', 'green')
-elif os.path.exists(home('.vim/UltiSnips')):
-    print colored('[Error]'.ljust(12), 'red'), '%-24s'%'UltiSnips', '|', colored('Directory Exists', 'green')
-else:
-    link_files('UltiSnips', home('.vim/UltiSnips'), sym=True)
-    print colored('[Unused]'.ljust(12), 'yellow'), '%-24s'%'UltiSnips', '|', colored('Linked', 'green')
-
-for src_file in glob.glob("files/*"):
-    name = os.path.basename(src_file)
-    dst_file = os.path.join(HOME, '.'+name)
-
-    if not os.path.exists(dst_file):
-        link_files(src_file, dst_file)
-        print colored('[Unused]'.ljust(12), 'yellow'), '%-24s'%name, '|', colored('Linked', 'green')
-        # print '|', colored('OK', 'green')
-        continue
-
+def is_linked(src_file, dst_file):
     src_stat = os.stat(src_file)
     dst_stat = os.stat(dst_file)
 
     if src_stat.st_ino == dst_stat.st_ino:
-        print colored('[Linked]'.ljust(12), 'green'), '%-24s'%name, '|', colored('No Action', 'green')
-        continue
+        return True
+    return False
+
+
+def diff_files(src_file, dst_file):
 
     if filecmp.cmp(src_file, dst_file, shallow=False):
-        link_files(src_file, dst_file, force=True)
-        print colored('[Unlinked]'.ljust(12), 'yellow'), '%-24s'%name, '|', colored('Similar files linked', 'green')
-        continue
-
+        return []
 
     src = open(src_file,'r')
     dst = open(dst_file,'r')
@@ -102,6 +76,9 @@ for src_file in glob.glob("files/*"):
                                 fromfile=src_file,
                                 tofile=dst_file,
                                 n=0)
+    src.close()
+    dst.close()
+
     difflines = []
     for line in diff:
         if len(line) >= 2 and line[:2] == '@@' or \
@@ -110,12 +87,56 @@ for src_file in glob.glob("files/*"):
            line.strip() == '':
             continue
         difflines.append(line.strip())
+    return difflines
+
+
+# TODO
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# TODO leave to vim?
+# curl -sfLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# TODO check the terminal and do various setup
+# TERM=xterm-256color-italic? (if xterm-256-color)
+# <BS> = C-?
+# Home/End/PU/PD work
+# Fkeys work
+
+# install vim plug
+# TODO: fix snippets
+
+system('mkdir -p "%s"'%home('.vim'))
+system('mkdir -p "%s"'%home('.vim/view'))
+system('mkdir -p "%s"'%home('.vim/undo'))
+
+# TODO check if exists
+# don't run, leave to bash/zsh?
+# system('tic "files/xterm-256color-italic.terminfo"')
+
+
+for src_file in glob.glob("files/*"):
+    name = os.path.basename(src_file)
+    dst_file = home('.'+name)
+
+    if not os.path.exists(dst_file):
+        print colored('[Unused]'.ljust(12), 'yellow'), '%-32s'%name, '|', colored('Linked', 'green')
+        system('ln "%s" "%s"', src_file, dst_file)
+        continue
+
+    if is_linked(src_file, dst_file):
+        print colored('[Linked]'.ljust(12), 'green'), '%-32s'%name, '|', colored('No Action', 'green')
+        continue
+    
+    difflines = diff_files(src_file, dst_file)
 
     if len(difflines) == 0:
-        link_files(src_file, dst_file, force=True)
-        print colored('[Unlinked]'.ljust(12), 'red'), '%-24s'%name, '|', colored('Similar files linked', 'yellow')
+        print colored('[Unlinked]'.ljust(12), 'red'), '%-32s'%name, '|', colored('Similar files linked', 'yellow')
+        system('rm -rf "%s"', dst_file)
+        system('ln "%s" "%s"', src_file, dst_file)
 
     elif len(difflines) < 10:
+        print colored('[Error]'.ljust(12), 'red'), '%-32s'%name, '|', colored('%s'%options[choice], 'yellow')
+
         print '-'*40, 'Local Diff:', name
         print '\n'.join(difflines)
         print '-'*40
@@ -124,38 +145,17 @@ for src_file in glob.glob("files/*"):
             'o':'Keep local modifications, overwrite source',
             'x':'Overwrite local modifications',
             's':'Skip',
-
         }
+
         choice = get_user_choice(options)
 
         if choice == 'o':
-            link_files(dst_file, src_file, force=True)
+            system('rm -rf "%s"', src_file)
+            system('ln "%s" "%s"', dst_file, src_file)
         elif choice == 'x':
-            link_files(src_file, dst_file, force=True)
-
-        print colored('[Error]'.ljust(12), 'red'), '%-24s'%name, '|', colored('%s'%options[choice], 'yellow')
+            system('rm -rf "%s"', src_file)
+            system('ln "%s" "%s"', src_file, dst_file)
 
     else:
-        print colored('[Abort]'.ljust(12), 'red'), '%-24s'%name, '|', colored('No Action', 'red')
+        print colored('[Abort]'.ljust(12), 'red'), '%-32s'%name, '|', colored('No Action', 'red')
 
-# #!/bin/bash
-#
-# cd "$(dirname "$0")"
-#
-# mkdir -p ~/.vim
-# mkdir -p ~/.vim/view
-# mkdir -p ~/.vim/undo
-#
-#
-# link() {
-#   echo "$1"
-#   rm -f "$HOME/.$1"
-#   ln "$1" "$HOME/.$1"
-# }
-#
-# # for f in *; do
-# # for f in $(find . type f); do
-# for f in $(find files -type f -printf "%f\n"); do
-#     link "$f"
-# done
-#
