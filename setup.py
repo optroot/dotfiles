e7184881c4e4ff21950c59aebb9a6c5ace70e0c9
optroot <optroot@gmail.com>
2017-02-22 11:38:44 -0500

    Fixed Formatting

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import getopt
 try:
     from termcolor import colored
 except:
-    colored = lambda x,y: x
+    colored = lambda x, y: x
 
 HOME = os.path.expanduser("~")
 settings = {
@@ -23,7 +23,7 @@ settings = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 optlist, args = getopt.getopt(sys.argv[1:], 'dv', ['dryrun', 'verbose'])
-for opt,arg in optlist:
+for opt, arg in optlist:
     if opt in ['-d', '--dryrun']:
         settings['dryrun'] = True
         settings['verbose'] = True
@@ -34,12 +34,17 @@ for opt,arg in optlist:
 def home(p):
     return os.path.join(HOME, p)
 
+
 def file_diff(src, dst):
     pass
 
+
 def system(cmd):
-    if settings['verbose']: print cmd
-    if not settings['dryrun']: os.system(cmd)
+    if settings['verbose']:
+        print cmd
+    if not settings['dryrun']:
+        os.system(cmd)
+
 
 def get_user_choice(choices):
     keys = choices.keys()
@@ -47,12 +52,15 @@ def get_user_choice(choices):
     first = True
     choice = None
     while first or choice not in keys:
-        if not first: print 'Invalid Choice'
+        if not first:
+            print 'Invalid Choice'
         for k in keys:
-            print '(%s): %s'%(str(k), choices[k])
-        choice = raw_input('Enter Option (%s): '%('/'.join(map(str, keys)).strip()))
+            print '(%s): %s' % (str(k), choices[k])
+        choice = raw_input('Enter Option (%s): ' %
+                           ('/'.join(map(str, keys)).strip()))
         first = False
     return choice
+
 
 def is_linked(src_file, dst_file):
     src_stat = os.stat(src_file)
@@ -68,8 +76,8 @@ def diff_files(src_file, dst_file):
     if filecmp.cmp(src_file, dst_file, shallow=False):
         return []
 
-    src = open(src_file,'r')
-    dst = open(dst_file,'r')
+    src = open(src_file, 'r')
+    dst = open(dst_file, 'r')
 
     diff = difflib.unified_diff(src.readlines(),
                                 dst.readlines(),
@@ -94,7 +102,8 @@ def diff_files(src_file, dst_file):
 # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # TODO leave to vim?
-# curl -sfLo ~/.vim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# curl -sfLo ~/.vim/autoload/plug.vim --create-dirs
+# 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # TODO check the terminal and do various setup
 # TERM=xterm-256color-italic? (if xterm-256-color)
@@ -105,9 +114,9 @@ def diff_files(src_file, dst_file):
 # install vim plug
 # TODO: fix snippets
 
-system('mkdir -p "%s"'%home('.vim'))
-system('mkdir -p "%s"'%home('.vim/view'))
-system('mkdir -p "%s"'%home('.vim/undo'))
+system('mkdir -p "%s"' % home('.vim'))
+system('mkdir -p "%s"' % home('.vim/view'))
+system('mkdir -p "%s"' % home('.vim/undo'))
 
 # TODO check if exists
 # don't run, leave to bash/zsh?
@@ -116,35 +125,35 @@ system('mkdir -p "%s"'%home('.vim/undo'))
 
 for src_file in glob.glob("files/*"):
     name = os.path.basename(src_file)
-    dst_file = home('.'+name)
+    dst_file = home('.' + name)
 
     if not os.path.exists(dst_file):
-        print colored('[Unused]'.ljust(12), 'yellow'), '%-32s'%name, '|', colored('Linked', 'green')
+        print colored('[Unused]'.ljust(12), 'yellow'), '%-32s' % name, '|', colored('Linked', 'green')
         system('ln "%s" "%s"', src_file, dst_file)
         continue
 
     if is_linked(src_file, dst_file):
-        print colored('[Linked]'.ljust(12), 'green'), '%-32s'%name, '|', colored('No Action', 'green')
+        print colored('[Linked]'.ljust(12), 'green'), '%-32s' % name, '|', colored('No Action', 'green')
         continue
-    
+
     difflines = diff_files(src_file, dst_file)
 
     if len(difflines) == 0:
-        print colored('[Unlinked]'.ljust(12), 'red'), '%-32s'%name, '|', colored('Similar files linked', 'yellow')
+        print colored('[Unlinked]'.ljust(12), 'red'), '%-32s' % name, '|', colored('Similar files linked', 'yellow')
         system('rm -rf "%s"', dst_file)
         system('ln "%s" "%s"', src_file, dst_file)
 
     elif len(difflines) < 10:
-        print colored('[Error]'.ljust(12), 'red'), '%-32s'%name, '|', colored('%s'%options[choice], 'yellow')
+        print colored('[Error]'.ljust(12), 'red'), '%-32s' % name, '|', colored('%s' % options[choice], 'yellow')
 
-        print '-'*40, 'Local Diff:', name
+        print '-' * 40, 'Local Diff:', name
         print '\n'.join(difflines)
-        print '-'*40
+        print '-' * 40
 
         options = {
-            'o':'Keep local modifications, overwrite source',
-            'x':'Overwrite local modifications',
-            's':'Skip',
+            'o': 'Keep local modifications, overwrite source',
+            'x': 'Overwrite local modifications',
+            's': 'Skip',
         }
 
         choice = get_user_choice(options)
@@ -157,5 +166,5 @@ for src_file in glob.glob("files/*"):
             system('ln "%s" "%s"', src_file, dst_file)
 
     else:
-        print colored('[Abort]'.ljust(12), 'red'), '%-32s'%name, '|', colored('No Action', 'red')
+        print colored('[Abort]'.ljust(12), 'red'), '%-32s' % name, '|', colored('No Action', 'red')
 
